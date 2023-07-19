574d2294c572084a3d63e0bea25578a78d5e3a4c
José Bonifácio <boni@Joses-MacBook-Pro.local>
2023-07-19 14:16:46 -0300

    views mensais

--- src/services/Admin/GetAdminUserService.ts
+++ src/services/Admin/GetAdminUserService.ts
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import prismaClient from '../../prisma'
 
 interface UserRequest {
@@ -27,6 +28,36 @@ class GetAdminUserService {
             }
         })
 
+        let filter = {
+            OR: []
+        }
+
+        let profiles = {}
+
+        user.profiles.map((item) => {
+            filter.OR.push({
+                profile_id: item.id
+            })
+            profiles[item.id] = 0
+        })
+
+        let date = new Date()
+
+        let views_month = await prismaClient.viewProfile.findMany({
+            where: {
+                ...filter,
+                month: format(date, "MM"),
+                year: format(date, "yyyy")
+                }
+        })
+        
+        views_month.map((item) => {
+            profiles[item.profile_id] += 1
+        })
+        user.profiles.map(async (item) => {
+            item["views_month"] = profiles[item.id]
+        })
+        
         return (user)
     }
 }
